248a832fce3da23f07a56ae2aadf23ff50239ce0
pauloc <pcandradeoliveira@hotmail.com>
2025-07-03 21:14:37 -0300

    Tipos de Variáveis

--- 1-tiposVariaveis/src/Main.java
+++ 1-tiposVariaveis/src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
 
+        int cep = 06704712;
+        byte idade = 123;
+        short ano = 2021;
+        long cpf = 50063614847L;//adicionamos no final o ( L ) maiúsculas.
+        float pi = 3.14F;//temos que colocar no final com ( F ) sé não ele acha que e um tipo double.
+        double salario = 1800.33;
+
+        System.out.println(cep);
+        System.out.println(idade);
+        System.out.println(ano);
+        System.out.println(cpf);
+        System.out.println(pi);
+        System.out.println(salario);
+
+
+
+
     }
 }
