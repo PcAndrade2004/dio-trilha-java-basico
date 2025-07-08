19aea6cf8491580ff596c7a94724cacc66dc94c7
PcAndrade2004 <pcandradeoliveira@hotmail.com>
2025-07-08 17:43:00 -0300

    teste
    adicionando

---
+++ 2-OPeradoresUNarios/src/decremento/Principal.java
@@ -0,0 +1,7 @@
+package decremento;
+
+public class Principal {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ 2-OPeradoresUNarios/src/incremento/Principal.java
@@ -0,0 +1,28 @@
+package incremento;
+
+public class Principal {
+    public static void main(String[] args) {
+        //temos duas formas de incrementar um número
+
+        //1 - forma.
+        int n1 = 5;
+        n1 = n1 + 1;//aqui ele incrementa um número, ou seja vai ficar 6
+        System.out.println(n1); // SAIDA : 6
+
+        // 2 - forma.
+        int numero = 10;
+        numero++; //ele incrementa o número
+        System.out.println(numero);// aqui ele exibe o valor ja atualizado.
+
+
+        //antecipando o incremento
+
+
+
+
+
+
+
+
+    }
+}
