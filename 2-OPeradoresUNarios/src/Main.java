ed82df30effe5ac9500052f1d6ddfa7307ca7971
PcAndrade2004 <pcandradeoliveira@hotmail.com>
2025-07-07 21:17:13 -0300

    Create Main.java
    Operador Unário. TRansformando número em negativo e positivo

---
+++ 2-OPeradoresUNarios/src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int numero = 5;
+        numero = - numero;//ele deixa o número negativo.
+        System.out.print(numero);
+
+        //deixando ele positivo
+        numero = numero * -1;//tornar o valor positivo.
+        System.out.print(numero);
+    }
+}
