ceadf3da71a829a75f92286517089f0bbd6f130f
PcAndrade2004 <pcandradeoliveira@hotmail.com>
2025-07-08 21:49:32 -0300

    Negando vlores booleanos
    Convertendo valores booleanos. Utiliznado o ( ! ) para inverter.

---
+++ 2-OPeradoresUNarios/src/negando_valor_booleano/Principal.java
@@ -0,0 +1,25 @@
+package negando_valor_booleano;
+
+public class Principal {
+    public static void main(String[] args) {
+        //valores booleanos tem duas alternativas (TRUE) OU (FALSE)
+        //mas podemos negar esses valores.
+
+        boolean variavel = true;//aqui incializamos a variável como (TRUE).
+        System.out.println(!variavel);//estamos negando, utilizando o ( ! ) que quando colocado antes da nossa variável,
+//         ele tem o papel de negar a nossa condição.
+
+        System.out.println("\nInvertendo o valor de uma variável booleana.");
+        variavel = !variavel;
+        System.out.println(variavel);
+
+
+
+
+
+
+
+
+
+    }
+}
