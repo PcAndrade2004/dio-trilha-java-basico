3e0381c9105ee9152ad3b1afa950c4dd9e0904d1
PcAndrade2004 <pcandradeoliveira@hotmail.com>
2025-07-08 21:50:21 -0300

    Decremento
    Decrementando valores utilizando o operador ( - - )

--- 2-OPeradoresUNarios/src/decremento/Principal.java
+++ 2-OPeradoresUNarios/src/decremento/Principal.java
@@ -3,5 +3,14 @@ package decremento;
 public class Principal {
     public static void main(String[] args) {
 
+        //1 - opção
+        int numero = 5;
+        numero = numero - 1;
+        System.out.println(numero);
+
+        //2 - opção
+        int numero1 = 10;
+        numero1--;
+        System.out.println(numero1);
     }
 }
