b1c1e47a65389fa76f8d02d72b5e5366839cf9d1
PauloCesar <pcandradeoliveira@hotmail.com>
2025-07-12 19:33:15 -0300

    Ternários
    Operadores Ternários

---
+++ 2-OPeradoresUNarios/src/ternários/Principal.java
@@ -0,0 +1,21 @@
+package ternários;
+
+public class Principal {
+    public static void main(String[] args) {
+
+        int a, b;
+        a = 55;
+        b = 20;
+
+        String resultado = a == b ? "Os números são iguais" : "Os números não são iguais.";
+        System.out.println(resultado);
+
+        //Primeiro adicionamos uma condição que precisa ter um retorno booleano.
+        //Depois colocamos qual será a (mensagem) ou o (número) que ira retornar caso seja (verdadeiro) ou (falso).
+
+        //lembrando que podemos adicionar tanto valor (Inteiros, double, float, boolean) é também podemos adicionar os tipos (‘String’)
+        // lembrando que seu funcionamento é igual ao condicional utilizando if/else.
+
+
+    }
+}
