921a555b1158f091a0cb7db627bd2f394835c648
PauloCesar <pcandradeoliveira@hotmail.com>
2025-07-12 22:11:01 -0300

    Operadores Relacionais
    Testando todos os operadores relacionais

---
+++ 2-OPeradoresUNarios/src/relacionais/Principal.java
@@ -0,0 +1,33 @@
+package relacionais;
+
+public class Principal {
+    public static void main(String[] args) {
+
+        //operador de ==
+        int numero1 = 10;
+        int numero2 = 10;
+
+        boolean simNao = numero1 == numero2;
+        System.out.println("O numero " + numero1 + " é igual ao " + numero2  + "? " + simNao);
+
+        //Operador de diferente
+         simNao = numero1 != numero2;
+        System.out.println(simNao);
+
+        //Operador MAIOR
+        simNao = numero1 > numero2;
+        System.out.println("O numero " + numero1 + " é MAIOR que o número " + numero2 + " ? " +  simNao);
+
+        //Operador MAIOR OU IGUAL
+        simNao = numero1 >= numero2;
+        System.out.println("O numero " + numero1 + " é MAIOR ou IGUAL ao número " + numero2 + " ? " +  simNao);
+
+        //Operador MENOR
+        simNao = numero1 < numero2;
+        System.out.println("O numero " + numero1 + " é MENOR que o número " + numero2 + " ? " +  simNao);
+
+        //Operador MENOR ou IGUAL
+        simNao = numero1 <= numero2;
+        System.out.println("O numero " + numero1 + " é MENOR ou IGUAL ao número " + numero2 + " ? " +  simNao);
+    }
+}
