9027e11cb144cc9038bc6bdaa820307697addc02
PauloCesar <pcandradeoliveira@hotmail.com>
2025-07-14 20:20:24 -0300

    operadores relacionais

--- 2-OPeradoresUNarios/src/relacionais/Principal.java
+++ 2-OPeradoresUNarios/src/relacionais/Principal.java
@@ -4,30 +4,42 @@ public class Principal {
     public static void main(String[] args) {
 
         //operador de ==
-        int numero1 = 10;
-        int numero2 = 10;
+//        int numero1 = 10;
+//        int numero2 = 10;
+//
+//        boolean simNao = numero1 == numero2;
+//        System.out.println("O numero " + numero1 + " é igual ao " + numero2  + "? " + simNao);
+//
+//        //Operador de diferente
+//         simNao = numero1 != numero2;
+//        System.out.println(simNao);
+//
+//        //Operador MAIOR
+//        simNao = numero1 > numero2;
+//        System.out.println("O numero " + numero1 + " é MAIOR que o número " + numero2 + " ? " +  simNao);
+//
+//        //Operador MAIOR OU IGUAL
+//        simNao = numero1 >= numero2;
+//        System.out.println("O numero " + numero1 + " é MAIOR ou IGUAL ao número " + numero2 + " ? " +  simNao);
+//
+//        //Operador MENOR
+//        simNao = numero1 < numero2;
+//        System.out.println("O numero " + numero1 + " é MENOR que o número " + numero2 + " ? " +  simNao);
+//
+//        //Operador MENOR ou IGUAL
+//        simNao = numero1 <= numero2;
+//        System.out.println("O numero " + numero1 + " é MENOR ou IGUAL ao número " + numero2 + " ? " +  simNao);
 
-        boolean simNao = numero1 == numero2;
-        System.out.println("O numero " + numero1 + " é igual ao " + numero2  + "? " + simNao);
 
-        //Operador de diferente
-         simNao = numero1 != numero2;
-        System.out.println(simNao);
+        double temperatura = 4.0;
+        boolean estaFrio = (temperatura < 5.0);
+        if(estaFrio) {
+            System.out.println("Hoje está frio.");
+        }else {
+            System.out.println("Hoje está um dia quente.");
+        }
 
-        //Operador MAIOR
-        simNao = numero1 > numero2;
-        System.out.println("O numero " + numero1 + " é MAIOR que o número " + numero2 + " ? " +  simNao);
+        
 
-        //Operador MAIOR OU IGUAL
-        simNao = numero1 >= numero2;
-        System.out.println("O numero " + numero1 + " é MAIOR ou IGUAL ao número " + numero2 + " ? " +  simNao);
-
-        //Operador MENOR
-        simNao = numero1 < numero2;
-        System.out.println("O numero " + numero1 + " é MENOR que o número " + numero2 + " ? " +  simNao);
-
-        //Operador MENOR ou IGUAL
-        simNao = numero1 <= numero2;
-        System.out.println("O numero " + numero1 + " é MENOR ou IGUAL ao número " + numero2 + " ? " +  simNao);
     }
 }
